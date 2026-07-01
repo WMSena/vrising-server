543b208920d9eff0d8fee4874a8dd91fc9d0ac70
WMSena <duisena10@gmail.com>
2026-07-01 14:08:31 +0200

    Commit VPS

--- apps/sebastian/src/config/index.js
+++ apps/sebastian/src/config/index.js
@@ -1,15 +1,16 @@
 export default {
 
     docker: {
-
         vrising: "vrising",
-
     },
-
+    mongo: {
+        uri: process.env.MONGO_URI
+    },
     backup: {
-
         directory: "/opt/vrising-server/backups/manual",
-
+    },
+    log: {
+        file: "/opt/vrising-server/data/persistent/latest.log"
     }
 
 };
